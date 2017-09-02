e04e84f7ede71b30083b2046b99e23fd486e0009
Novikov Viktor <viktor.novikov95@gmail.com>
2017-09-02 09:09:41 +0300

    Added provider interface

---
+++ Storage/Provider/ProviderInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Describes methods Provider class should contain
+ */
+interface ProviderInterface
+{
+    /**
+     * Fetch item from storage by key
+     *
+     * @param $key
+     * @return mixed
+     */
+    public function get($key);
+
+    /**
+     * Set item to storage with defined key and value($expiresAt)
+     *
+     * @param $key
+     * @param $expiresAt
+     * @return mixed
+     */
+    public function set($key, $expiresAt);
+}
